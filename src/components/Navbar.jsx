d3503b6ea091831fbbba1c36c8467994235f6b44
tamayo2 <juan.tamayo3@utp.edu.co>
2025-03-12 16:11:38 -0500

    Noche

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { AiOutlineMenu } from "react-icons/ai";
+import logo from "../../public/logo.png"
 
 function Navbar() {
     return (
-        <div className="flex justify-between px py-2" >
-            <div className="border border-black">
+        <div className="flex justify-between px-6 py-2" >
+            <div className="flex items-center border border-black">
                 <AiOutlineMenu />
-                Logo
+                <img src={logo} alt="Logo" className="w-28" />
             </div>
             <div className="border border-black">2</div>
             <div className="border border-black">3</div>
