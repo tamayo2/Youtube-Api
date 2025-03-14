8b0a886ce53c2150c95afa626300f5cf1969f7d7
tamayo2 <juan.tamayo3@utp.edu.co>
2025-03-14 01:03:14 -0500

    Inicializacion del Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from "./components/Navbar.jsx";
+import Sidebar from "./components/Sidebar.jsx";
 
 function App() {
     return (
         <div className="App">
             <Navbar />
+            <Sidebar />
         </div>
     )
 }
